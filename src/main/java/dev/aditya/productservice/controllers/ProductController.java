aec520aa0ae45f1662bb6054c9741673d821c451
Aditya Mohan <adimohan93@gmail.com>
2023-09-30 21:56:23 +0530

    FIrst commit
    MVP with hardcoded REST api responses

---
+++ src/main/java/dev/aditya/productservice/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package dev.aditya.productservice.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public void getAllProducts() {
+
+    }
+
+    // localhost:8080/products/123
+    @GetMapping("{id}")
+    public String getProductById(@PathVariable("id") Long id) {
+        return "Here is product id: " + id;
+    }
+    @DeleteMapping("{id}")
+    public void deleteProductById() {
+
+    }
+    @PostMapping
+    public void createProduct() {
+
+    }
+    @PutMapping("{id}")
+    public void updateProductById() {
+
+    }
+}
---
+++ src/main/java/dev/aditya/productservice/models/Category.java
@@ -0,0 +1,5 @@
+package dev.aditya.productservice.models;
+
+public class Category {
+    private String name;
+}
